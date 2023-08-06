8a3e6786f60d7b5c83c9ed2d37a3f4fab39717c4
danilsab24 <sabatini.1890300@studenti.uniroma1.it>
2023-08-06 11:14:41 +0200

    modifiche dataset

--- dataset.py
+++ dataset.py
@@ -18,12 +18,35 @@ class Data(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
         label_path = os.path.join(self.label_dir, self.images[index].replace("_IRRG.tif","_label.tif"))
-        image = np.array(Image.open(img_path).convert("IRRG"))
-        label = np.array(Image.open(label_path).convert("RGB"), dtype=np.float64)
+        image = np.array(Image.open(img_path))
+        label = np.array(Image.open(label_path).convert("RGB"), dtype=np.uint8)
+        
+        # Define class mappings based on RGB values
+        class_mapping = {
+            (255, 255, 255): 0,  # Impervious surfaces
+            (0, 0, 255): 1,      # Building
+            (0, 255, 255): 2,    # Low vegetation
+            (0, 255, 0): 3,      # Tree
+            (255, 255, 0): 4,    # Car
+            (255, 0, 0): 5      # Clutter/background
+        }
+        
+        # Convert RGB label to class indices
+        label_indices = np.zeros(label.shape[:2], dtype=np.uint8)
+        for rgb, class_idx in class_mapping.items():
+            mask = np.all(label == np.array(rgb), axis=-1)
+            label_indices[mask] = class_idx
         
         if self.transform is not None:
-            augmentations = self.transform(image=image, label=label)
+            augmentations = self.transform(image=image, label=label_indices)
             image = augmentations["image"]
-            label = augmentations["label"]
+            label_indices = augmentations["label"]
         
-        return image, label
+        return image, label_indices
+
+
+
+
+
+
+
