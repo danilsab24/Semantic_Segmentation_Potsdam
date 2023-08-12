551965b137a5cd76ed1c76806f54f35126bf3013
danilsab24 <sabatini.1890300@studenti.uniroma1.it>
2023-08-12 11:32:57 +0200

    modifiche dataset

--- dataset.py
+++ dataset.py
@@ -2,32 +2,33 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
-from skimage.util.shape import view_as_windows
-
 
 class Data(Dataset):
-    def __init__(self, image_dir, label_dir, transform=None):
-        self.image_dir = image_dir
-        self.label_dir = label_dir
+    def __init__(self, data_root, target_root, transform=None):
+        self.data_root = data_root
+        self.target_root = target_root
         self.transform = transform
-        self.image_filenames = [filename for filename in os.listdir(image_dir) if filename.endswith('.tif')]
+        self.data_filenames = os.listdir(data_root)
+        self.data_filenames.sort()  # Ordina i nomi dei file
         
     def __len__(self):
-        return len(self.images)
+        return len(self.data_filenames)
     
-    def __getitem__(self, index):
-        image_path = os.path.join(self.images_dir, self.image_filenames[index])
-        label_filename = self.image_filenames[index].replace("_IRRG", "_label")
-        label_path = os.path.join(self.labels_dir, label_filename)
+    def __getitem__(self, idx):
+        #data_filename = self.data_filenames[idx].replace("_IRRG", "")  # Rimuovi "_IRRG" dal nome del file
+        #target_filename = data_filename.replace("_label", "") 
+        data_path = os.path.join(self.data_root, self.data_filenames[idx])
+        target_path = os.path.join(self.target_root, self.data_filenames[idx])
         
-        image = Image.open(image_path)
-        label = Image.open(label_path)
+        data_image = Image.open(data_path).convert('RGB')
+        target_image = Image.open(target_path).convert('RGB')  # Keep as RGB for multi-class
         
         if self.transform:
-            image = self.transform(image)
-            label = self.transform(label)
-        
-        return image, label
+            data_image = self.transform(data_image)
+            target_image = self.transform(target_image)
+            
+        return data_image, target_image
+
 
 
 
